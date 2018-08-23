42903292ea22b072184fc58525b5bdf1321a52cd
unknown <you@example.com>
2018-08-23 23:42:38 +1000

    Make navbar view conditional on user logged in

--- Payroll/Controllers/HomeController.cs
+++ Payroll/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace Payroll.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your timesheets.";
 
             return View();
         }
